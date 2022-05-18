279bf572f26e077844b89897248cdf2d375e5f5a
ILIIH <jointlord0@gmail.com>
2022-05-18 20:59:06 +0300

    added recyclerview

--- app/src/main/java/com/example/gitapp/Presentation/LoginFragment.kt
+++ app/src/main/java/com/example/gitapp/Presentation/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.gitapp.R
+import com.example.gitapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
@@ -15,8 +17,14 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val bindidg = FragmentLoginBinding.inflate(inflater)
+
+        bindidg.EnterButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_profileFragment)
+
+        }
+
+        return bindidg.root
     }
 
 
