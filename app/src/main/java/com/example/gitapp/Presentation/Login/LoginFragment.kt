581e442325b0b7df54d0697ec850a48c6e4750bc
ILIIH <jointlord0@gmail.com>
2022-05-24 18:59:10 +0300

    fixed Login UI

--- app/src/main/java/com/example/gitapp/Presentation/Login/LoginFragment.kt
+++ app/src/main/java/com/example/gitapp/Presentation/Login/LoginFragment.kt
@@ -1,18 +1,25 @@
 package com.example.gitapp.Presentation.Login
 
+import android.R
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.core.Domain.helpers.ErrorEntity
+import com.example.core.Domain.helpers.Result
 import com.example.gitapp.databinding.FragmentLoginBinding
 import com.example.gitapp.di.MyApplication
+import com.example.gitapp.util.hideKeyboard
 import javax.inject.Inject
-import com.example.core.Domain.helpers.Result
 
 
 class LoginFragment : Fragment() {
@@ -31,6 +38,29 @@ class LoginFragment : Fragment() {
 
         (requireActivity().applicationContext as MyApplication).appComponent.LoginInject(this)
 
+
+        bindidg.AuthTokenTextField.onCheckIsTextEditor()
+
+
+
+        bindidg.UserNameTextField.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                bindidg.AuthTokenTextField.requestFocus()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+
+        bindidg.AuthTokenTextField.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                bindidg.EnterButton.requestFocus()
+                hideKeyboard(requireActivity());
+                return@OnEditorActionListener true
+            }
+            false
+        })
 
         ViewModel =  ViewModelProvider(this,vmFactory).get(LoginViewModel::class.java)
 
@@ -56,6 +86,8 @@ class LoginFragment : Fragment() {
 
         return bindidg.root
     }
+
+
 
 
 }
